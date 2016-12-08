ade9d0caefd11556391e07fdd40e102cfcef7422
ericel <ericel123new@gmail.com>
2016-12-08 21:17:43 +0900

    Star rating ng2-rating Added

--- src/app/places/place/place.component.ts
+++ src/app/places/place/place.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place.component.css']
 })
 export class PlaceComponent implements OnInit {
-lat: number = 51.678418;
+  lat: number = 51.678418;
   lng: number = 7.809007;
+
+  starsCount: number;
+  starsCounts: number[] = [];
   constructor() { }
 
   ngOnInit() {
+  	this.starsCount = 2.5;
   }
 
 }
--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
 import { PlacesComponent } from './places.component';
 import { AgmCoreModule } from 'angular2-google-maps/core';
@@ -9,16 +10,20 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PlaceComponent } from './place/place.component';
 import { AdsenseModule } from 'ng2-adsense';
+import {RatingModule} from "ng2-rating";
+
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     PlacesRouting,
     AdsenseModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBqIrNb1DLsN6oP97ua3YLMJx5-gUueWJU'
     }),
-     MaterialModule.forRoot()
+     MaterialModule.forRoot(),
+    RatingModule
   ],
   providers: [
   
